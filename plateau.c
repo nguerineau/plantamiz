726f99da897fa7890840a635d503265b8d8285cb
nguerineau <nicolas@guerineau.org>
2024-10-20 15:53:30 +0200

    bug elimination

---
+++ plateau.c
@@ -0,0 +1,16 @@
+//
+// Created by nicol on 10/20/2024.
+//
+
+#include "plateau.h"
+
+void init_plateau() {
+
+
+}
+
+void display_plateau() {
+
+
+}
+
---
+++ plateau.h
@@ -0,0 +1,14 @@
+//
+// Created by nicol on 10/20/2024.
+//
+
+#ifndef PLATEAU_H
+#define PLATEAU_H
+//taille tableau
+#define longueur 45
+#define hauteur 25
+
+//
+int plateau[longueur][hauteur];
+
+#endif //PLATEAU_H
