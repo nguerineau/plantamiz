2df2e1151c49e305f8e6b5b683202ee57530abc8
Ayyub92i <ayyub.belchguer@edu.ece.fr>
2024-11-26 13:29:55 +0100

    code sauvegarde

--- sauvegarde.c
+++ sauvegarde.c
@@ -1,5 +1,19 @@
-//
-// Created by ayyub on 26/11/2024.
-//
+#include "sauvegarde.h"//
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-#include "sauvegarde.h"
+void sauvegarde(char nom[], int progression) {
+    int i = 0;
+    FILE* pf = fopen("../monFichier.txt", "w");
+    if (pf == NULL) {
+        printf("Erreur d'ouverture de fichier.");
+    }
+
+
+    fclose(pf);
+    pf = NULL;
+
+}
+// Created by ayyub on 25/11/2024.
+//
--- sauvegarde.h
+++ sauvegarde.h
@@ -1,8 +1,9 @@
 //
-// Created by ayyub on 26/11/2024.
+// Created by ayyub on 25/11/2024.
 //
 
 #ifndef SAUVEGARDE_H
 #define SAUVEGARDE_H
+void sauvegarde();
 
 #endif //SAUVEGARDE_H
