ba27ccc654762a67e48521528c38183960807e01
nguerineau <nicolas@guerineau.org>
2024-12-07 12:55:16 +0100

    create game function

---
+++ game function.c
@@ -0,0 +1,5 @@
+//
+// Created by nicol on 07/12/2024.
+//
+
+#include "game function.h"
---
+++ game function.h
@@ -0,0 +1,8 @@
+//
+// Created by nicol on 07/12/2024.
+//
+
+#ifndef GAME_FUNCTION_H
+#define GAME_FUNCTION_H
+
+#endif //GAME_FUNCTION_H
